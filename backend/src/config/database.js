6a0bab92d986d9f670defc6bb70edae5e199dcef
Roshan Patil <patilroshan7768@gmail.com>
2026-05-29 19:29:13 +0530

    Suphabase correct

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -9,12 +9,11 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
 );
